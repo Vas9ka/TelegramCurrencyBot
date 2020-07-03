74c478c29cdbac1e6d715a7a5fc9e942b6537055
Yugay Vasiliy <31739813+Vas9ka@users.noreply.github.com>
2020-07-03 16:22:48 +0300

    Update test_pytest.py

--- test_pytest.py
+++ test_pytest.py
@@ -4,4 +4,4 @@ def test_date_and_currency_cb():
     assert CentralBank.GetCurrency("UZM","20-03-2000") == None
 def test_currency_cb():
     assert CentralBank.GetCurrency("USD") == "Курсы валют ЦБ РФ на сегодня (USD)\n70.5198 руб."
-    assert CentralBank.GetCurrency("UZM","20-03-2000") == None
+    assert CentralBank.GetCurrency("UZM") == None
